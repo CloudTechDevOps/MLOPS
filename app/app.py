05a917d3a587db08fa4a9f18d4c676d3dbdec34b
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-07-26 16:23:38 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ def dummy_predict(x):
 
 @app.route('/')
 def index():
-    return "Welcome to the ML Appp veera"
+    return "Welcome to the ML Appp veera12"
 
 @app.route('/predict', methods=['GET'])
 def predict():
